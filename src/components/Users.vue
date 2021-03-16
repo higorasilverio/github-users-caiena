8396feedb011b531fe08208ea9167b27c87faa5f
higorasilverio <59712418+higorasilverio@users.noreply.github.com>
2021-03-15 22:55:12 -0300

    Field to search users inserted and methods rearranged

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -3,16 +3,24 @@
     <b-modal id="modal-get-access-token" centered hide-footer hide-header no-close-on-esc no-close-on-backdrop>
       <h5>Enter the access token:</h5>
       <b-input-group>
-        <b-form-input type="password" placeholder="ACCESS TOKEN" v-model="token" @keypress.enter="take" />
+        <b-form-input type="password" placeholder="ACCESS TOKEN" v-model="token" @keypress.enter="initiate" />
         <b-input-group-append is-text>
-          <b-icon icon="patch-check" @click="take"/>
+          <b-icon icon="patch-check" @click="initiate" />
         </b-input-group-append>
       </b-input-group>
     </b-modal>
     <b-modal id="modal-show-github-user" size="xl" centered hide-footer hide-header no-close-on-esc no-close-on-backdrop>
       <b-container fluid>
         <b-row>
-          <b-col cols="6" v-for="user in users" :key="user.userName">
+          <b-input-group class="mb-5">
+            <b-input-group-prepend is-text>
+              <b-icon icon="search" @click="getUser" />
+            </b-input-group-prepend>
+            <b-form-input type="search" placeholder="Search user" v-model="user" @keypress.enter="getUser" />
+          </b-input-group>
+        </b-row>
+        <b-row>
+          <b-col cols="6" v-for="(user, index) in users" :key="index">
             <b-row>
               <b-col cols="2"><b-img-lazy :src="user.userAvatar" v-bind="imgProps" alt="User avatar"></b-img-lazy></b-col>
               <b-col cols="3">{{user.userName}}</b-col>
@@ -20,8 +28,10 @@
             </b-row>
           </b-col>
         </b-row>
-        <label for="number">Page:</label>
-        <b-form-spinbutton id="number" v-model="pageNumber" inline min="1" max="120"/>
+        <b-row class="mt-5">
+          <label for="number">Page:</label>
+          <b-form-spinbutton id="number" v-model="pageNumber" inline min="1" max="120"/>
+        </b-row>
       </b-container>
     </b-modal>
   </div>
@@ -34,6 +44,7 @@ export default {
     return {
       token: '',
       users: [],
+      user: '',
       fields: [
         { key: 'userAvatar', label: '' },
         { key: 'userName', label: 'User' },
@@ -56,28 +67,50 @@ export default {
   },
   watch: {
     pageNumber: function () {
-      this.take()
+      this.changePage()
     }
   },
   methods: {
-    take () {
+    initiate () {
       this.$bvModal.hide('modal-get-access-token')
-      this.user = []
-      this.axios
+      this.getUsers() ? this.$bvModal.show('modal-show-github-user') : this.$bvModal.show('modal-get-access-token')
+    },
+    changePage () {
+      this.users.length = 0
+      this.getUsers() ? this.$bvModal.show('modal-show-github-user') : this.pageNumber--
+    },
+    async getUsers () {
+      await this.axios
         .get(`https://api.github.com/users?since=${this.pageNumber - 1}&per_page=20`, {
           headers: {
-            Authorization: `token ${this.token}`
+            Authorization: `token ${this.token}`,
+            Accept: 'application/vnd.github.v3+json'
           }
         })
-        .then(res => {
-          res.data.forEach(element => {
+        .then(response => {
+          response.data.forEach(element => {
             this.users.push({userAvatar: element.avatar_url, userName: element.login, userUrl: element.html_url})
           })
-          this.$bvModal.show('modal-show-github-user')
+          return true
         })
         .catch(error => {
           console.error(error)
-          this.$bvModal.show('modal-get-access-token')
+          return false
+        })
+    },
+    async getUser () {
+      await this.axios
+        .get(`https://api.github.com/users/${this.user}`, {
+          headers: {
+            Authorization: `token ${this.token}`,
+            Accept: 'application/vnd.github.v3+json'
+          }
+        })
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => {
+          console.error(error)
         })
     }
   }
