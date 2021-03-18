cfd92d7ab2d8423fdee9a620d2d135c6d3ba6732
higorasilverio <higorasilverio@outlook.com>
2021-03-18 01:37:34 -0300

    validation(home): validation inserted on token and user search inputs

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,50 +3,64 @@
     <b-modal id="modal-get-access-token" centered hide-footer hide-header no-close-on-esc no-close-on-backdrop>
       <h5>Enter the access token:</h5>
       <b-input-group>
-        <b-form-input type="password" placeholder="ACCESS TOKEN" v-model="token" @keypress.enter="initiate" />
+        <b-form-input :state="stateToken" type="password" placeholder="ACCESS TOKEN"
+          v-model="token" @keypress.enter="initiate" @focus="stateToken = null"
+          aria-describedby="input-live-feedback-token"/>
         <b-input-group-append is-text>
           <b-icon icon="patch-check" @click="initiate" />
         </b-input-group-append>
+        <b-form-invalid-feedback id="input-live-feedback-token">
+          Invalid token
+        </b-form-invalid-feedback>
       </b-input-group>
     </b-modal>
     <b-modal id="modal-show-github-user" size="xl" centered hide-footer hide-header no-close-on-esc no-close-on-backdrop>
-      <b-container fluid>
-        <b-row align-h="end">
-          <b-col sm="6" md="6" lg="4" xl="3">
-            <b-input-group class="mb-3">
-              <b-input-group-prepend is-text>
-                <b-icon icon="search" @click="getSingleUser" />
-              </b-input-group-prepend>
-              <b-form-input type="search" placeholder="Search user" v-model="user" @keypress.enter="getSingleUser" />
-            </b-input-group>
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col cols="6" v-for="user in users" :key="user.userId">
-            <User :userName="user.userName" :userAvatar="user.userAvatar" :userUrl="user.userUrl" />
-          </b-col>
-        </b-row>
-        <b-row class="mt-2 mr-1" align-h="end">
-          <div class="flex-pagination-div">
-            <span>Page</span>
-            <b-form-spinbutton id="number" v-model="pageNumber" inline min="1" max="120"/>
-          </div>
-        </b-row>
-      </b-container>
+      <b-overlay :show="charge" rounded="sm">
+        <b-container fluid>
+          <b-row align-h="end">
+            <b-col sm="6" md="6" lg="4" xl="3">
+              <b-input-group class="mb-3">
+                <b-input-group-prepend is-text>
+                  <b-icon icon="search" @click="getSingleUser" />
+                </b-input-group-prepend>
+                <b-form-input :state="stateUserName" type="search" placeholder="Search user"
+                  v-model="user" @keypress.enter="getSingleUser" @focus="stateUserName = null"
+                  aria-describedby="input-live-feedback-user"/>
+                <b-form-invalid-feedback id="input-live-feedback-user">
+                  User not found
+                </b-form-invalid-feedback>
+              </b-input-group>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col cols="6" v-for="user in users" :key="user.userId">
+              <User :userName="user.userName" :userAvatar="user.userAvatar" :userUrl="user.userUrl" />
+            </b-col>
+          </b-row>
+          <b-row class="mt-2 mr-1" align-h="end">
+            <div class="flex-pagination-div">
+              <span>Page</span>
+              <b-form-spinbutton id="number" v-model="pageNumber" inline min="1" max="120"/>
+            </div>
+          </b-row>
+        </b-container>
+      </b-overlay>
     </b-modal>
     <b-modal id="modal-show-single-user" centered hide-footer hide-header no-close-on-esc no-close-on-backdrop>
-      <b-container fluid>
-        <b-row>
-          <b-col>
-            <SingleUser :userName="singleUser.userName" :userAvatar="singleUser.userAvatar" :userUrl="singleUser.userUrl" />
-          </b-col>
-        </b-row>
-        <b-row align-h="center">
-          <b-button variant="outline-dark" @click="closeSingleUser">
-            <span class="button-return"><b-icon icon="arrow-return-left" /> Return</span>
-          </b-button>
-        </b-row>
-      </b-container>
+      <b-overlay :show="charge" rounded="sm">
+        <b-container fluid>
+          <b-row>
+            <b-col>
+              <SingleUser :userName="singleUser.userName" :userAvatar="singleUser.userAvatar" :userUrl="singleUser.userUrl" />
+            </b-col>
+          </b-row>
+          <b-row align-h="center">
+            <b-button variant="outline-dark" @click="closeSingleUser">
+              <span class="button-return"><b-icon icon="arrow-return-left" /> Return</span>
+            </b-button>
+          </b-row>
+        </b-container>
+      </b-overlay>
     </b-modal>
   </div>
 </template>
@@ -72,7 +86,10 @@ export default {
         { key: 'userName', label: 'User' },
         { key: 'userUrl', label: "User's profile" }
       ],
-      pageNumber: 1
+      pageNumber: 1,
+      charge: false,
+      stateToken: null,
+      stateUserName: null
     }
   },
   mounted () {
@@ -86,13 +103,14 @@ export default {
   methods: {
     initiate () {
       this.$bvModal.hide('modal-get-access-token')
-      this.getUsers() ? this.$bvModal.show('modal-show-github-user') : this.$bvModal.show('modal-get-access-token')
+      this.getUsers()
     },
     changePage () {
       this.users.length = 0
-      this.getUsers() ? this.$bvModal.show('modal-show-github-user') : this.pageNumber--
+      this.getUsers()
     },
     async getUsers () {
+      this.charge = true
       await this.axios
         .get(`https://api.github.com/users?since=${(this.pageNumber - 1) * 20}&per_page=20`, {
           headers: {
@@ -104,14 +122,18 @@ export default {
           response.data.forEach(element => {
             this.users.push({userId: element.id, userAvatar: element.avatar_url, userName: element.login, userUrl: element.html_url})
           })
-          return true
+          this.$bvModal.show('modal-show-github-user')
+          this.charge = false
         })
         .catch(error => {
           console.error(error)
-          return false
+          this.stateToken = false
+          this.$bvModal.show('modal-get-access-token')
+          this.charge = false
         })
     },
     async getSingleUser () {
+      this.charge = true
       await this.axios
         .get(`https://api.github.com/users/${this.user}`, {
           headers: {
@@ -128,9 +150,13 @@ export default {
           }
           this.$bvModal.hide('modal-show-github-user')
           this.$bvModal.show('modal-show-single-user')
+          this.charge = false
         })
         .catch(error => {
           console.error(error)
+          this.$bvModal.show('modal-show-github-user')
+          this.stateUserName = false
+          this.charge = false
         })
     },
     closeSingleUser () {
